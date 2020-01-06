25c4218b4ffa70d67accfa162ab68657f9d938ae
fanxinxian <you@example.com>
2020-01-06 10:15:21 +0800

    项目

---
+++ demo-vue/src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="login"></div>
+</template>
+<script>
+export default {
+    props: {},
+    components: {},
+    data() {
+        return {};
+    },
+    computed: {},
+    methods: {},
+    created() {},
+    mounted() {}
+};
+</script>
+<style scoped lang="scss">
+.login{
+    width: 100%;
+    height: 100%;
+    background:-webkit-gradient(linear, 100% 0%, 0% 0%, from(#ffffff),color-stop(0.5,#00f76f),to(#00e1ff));
+}
+</style>
