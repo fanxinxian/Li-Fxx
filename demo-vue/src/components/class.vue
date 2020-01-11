374a7250d671eb83b17a7608403aca0470a444c5
lijiang <1530553291@qq.com>
2020-01-11 08:47:02 +0800

    dev

---
+++ demo-vue/src/components/class.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="class">
+        <span>我的班级</span>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+
+    },
+    components:{
+
+    },
+    data(){
+        return {
+
+        }
+    },
+    computed:{
+
+    },
+    methods:{
+
+    },
+    created(){
+
+    },
+    mounted(){
+
+    }
+}
+</script>
+<style scoped lang="">
+
+</style>
